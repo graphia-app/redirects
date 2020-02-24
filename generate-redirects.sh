ca993ddb9fabffbeca374ab83fa9e81dbe56c90c
Tim Angus <tim@ngus.net>
2020-02-24 18:30:14 +0000

    Add existing redirects from kajeka.com

--- generate-redirects.sh
+++ generate-redirects.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
 declare -A REDIRECTS=(
+    ['coef_variation']='https://en.wikipedia.org/wiki/Coefficient_of_variation'
+    ['contraction']='https://en.wikipedia.org/wiki/Edge_contraction'
+    ['eccentricity']='https://en.wikipedia.org/wiki/Eccentricity_(graph_theory)'
+    ['help']='https://kajeka.com/wiki/Graphia:Home'
+    ['knn']='https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm'
+    ['louvain']='https://en.wikipedia.org/wiki/Louvain_modularity'
+    ['mcl']='https://micans.org/mcl/'
+    ['pagerank']='https://en.wikipedia.org/wiki/PageRank'
+    ['pearson']='https://en.wikipedia.org/wiki/Pearson_correlation_coefficient'
     ['regex']='https://perldoc.perl.org/perlre.html'
+    ['register']='https://kajeka.com/graphia-download'
+    ['spanning_tree']='https://en.wikipedia.org/wiki/Spanning_tree'
+    ['spearman']='https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient'
+    ['stddev']='https://en.wikipedia.org/wiki/Standard_deviation'
+    ['variance']='https://en.wikipedia.org/wiki/Variance'
 )
 
 
